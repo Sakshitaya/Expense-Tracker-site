c0a3f737a5c8911919ab9280f7f6d32cdc65cef9
Sakshi@22 <156348633+Sakshitaya@users.noreply.github.com>
2026-06-05 10:57:34 +0530

    Refactor connectDB function in Database.js
    Refactor connectDB function to remove parameters and improve error handling.

--- Expense-Tracker/backend/DB/Database.js
+++ Expense-Tracker/backend/DB/Database.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectDB = async (req, res) => {
+export const connectDB = async () => {
+  try {
     const db = process.env.MONGO_URL;
 
-    const {connection} = await mongoose.connect(db, { useNewUrlParser: true });
+    const { connection } = await mongoose.connect(db);
 
     console.log(`MongoDB Connected to ${connection.host}`);
-
-}
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
+};
